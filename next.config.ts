047a2221b5ac1b6b5a2a6859fbc4bda3a48047fd
gharzouli zakaria Ms <zakwissal77@gmail.com>
2026-09-01 15:01:20 +0200

    Configuration et optimisation pour déploiement Vercel (Next.js + Prisma)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   reactStrictMode: false,
 };
